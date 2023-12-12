8cc8301cd4099020f9d039dd4aa8ed5b27b87032
Garrett Bowman <garrettbowman248@gmail.com>
2023-12-12 14:34:26 -0600

    Feat: Notes from lecture

---
+++ src/TypesVariablesSyntaxLecture.java
@@ -0,0 +1,28 @@
+public class TypesVariablesSyntaxLecture {
+    public static void main(String[] args) {
+        System.out.println("This is a statement");
+        System.out.println("it ends with a semicolon");
+        System.out.println("STATEMENTS MUST END WITH SEMICOLON OR WORLD ENDS");
+
+
+//        DATA TYPES
+        byte smallNum = 19;
+        System.out.println(smallNum);
+        smallNum = 127;
+        smallNum++;
+        System.out.println(smallNum);
+
+        char firstLetterOfName = 'G'; // includes 1 single number character
+
+        String message = "Java String Needs DOUBLE QUOTES";
+        String message2 = "\"Java String Needs DOUBLE QUOTE\" Escaping the quote";
+        System.out.println(message);
+        System.out.println(message2);
+
+        final String SCREAMING_SNAKE = "const is in screaming snake";
+
+
+
+
+    }
+}
