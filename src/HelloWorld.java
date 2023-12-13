50b1a56536939a3c57b0117ae63f7b3bd8fb3c2c
Garrett Bowman <garrettbowman248@gmail.com>
2023-12-13 09:50:19 -0600

    Feat: Exercise review

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,44 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        int myFavNum = 3;
+        System.out.println(myFavNum);
+
+        String myString = "String";
+        System.out.println(myString);
+
+//        myString = 'S';
+//        myString = 3.14159;
+        long myNum;
+//        myNum = 123L;
+//        float myNum = 3.14F;
+//        int myNum = 123;
+
+        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5
+
+//        int x = 3;
+//        int y = 2;
+//        y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x:
+
+        int bigInt = Integer.MAX_VALUE + 1;
+        System.out.println(bigInt);
+
+
     }
 }
