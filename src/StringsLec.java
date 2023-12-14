b963423eff2d05e9e4e04a18910554bb6271621b
Garrett Bowman <garrettbowman248@gmail.com>
2023-12-14 15:14:45 -0600

    Feat: String Lecture Notes

---
+++ src/StringsLec.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class StringsLec {
+    public static void main(String[] args) {
+        System.out.println("con" + "cat");
+
+        String con = "con";
+        System.out.println(con.concat("cat"));
+        System.out.println("con".concat("cat"));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("whats your name?");
+        String name = scanner.next();
+        if(name.startsWith("G")){
+            System.out.println("mine starts with G aswell");
+        }
+        if(name.endsWith("t")){
+            System.out.println("mine ends with t aswell");
+        }
+        if(name.startsWith("G") && name.endsWith("t") && name.equals("Garrett")) {
+            System.out.println("We have the same name");
+        }
+        if(name.startsWith("G") && name.endsWith("t") && !name.equals("Garrett")) {
+            System.out.println("Thats a wierd way to spell Garrett");
+        }
+
+
+        String sample = "\nI Like Turtles.   Donkey Rattle.   ";
+        System.out.println(sample.charAt(9));
+        System.out.println(sample.substring(1,16)); // noninclusive max
+        System.out.println(sample.indexOf("turtles"));
+        System.out.println(sample.lastIndexOf("."));
+        System.out.println(sample.length());
+        System.out.println(sample.replace("rattle", "kong"));
+        System.out.println(sample.toLowerCase());
+        System.out.println(sample.toUpperCase());
+        System.out.println(sample.trim());
+
+
+
+
+
+
+
+
+    }
+}
