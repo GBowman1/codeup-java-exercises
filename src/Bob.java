2b7d845e8ed7c78fe3fddba5b1a79612beacbef3
Garrett Bowman <garrettbowman248@gmail.com>
2023-12-15 09:42:59 -0600

    Feat: Fixed bob to loop

--- src/Bob.java
+++ src/Bob.java
@@ -5,16 +5,21 @@ public class Bob {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ask bob a question");
+        while (true){
         String userPrompt = scanner.nextLine();
 
         if (userPrompt.endsWith("?")) {
-            System.out.println("Sure");
+            System.out.println("Sure.");
         } else if (userPrompt.endsWith("!")) {
             System.out.println("Whoa, chill out!");
         } else if (userPrompt.equals("")) {
             System.out.println("Fine. Be that way!");
+        } else if (userPrompt.equals("stop")) {
+            break;
         } else {
             System.out.println("Whatever.");
+        }
+
         }
     }
 }
